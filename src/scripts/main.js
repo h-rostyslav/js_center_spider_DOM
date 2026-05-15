92333ee88c51355960dda87d1e9f6155f21703bf
Rostyslav` <hrostyslav@ukr.net`>
2026-05-15 14:08:05 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+function centerSpider() {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+
+  const wallWidth = wall.offsetWidth;
+  const wallHeight = wall.offsetHeight;
+
+  const centerX = (wallWidth - spiderWidth) / 2;
+  const centerY = (wallHeight - spiderHeight) / 2;
+
+  spider.style.left = `${centerX}px`;
+  spider.style.top = `${centerY}px`;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  centerSpider();
+});
