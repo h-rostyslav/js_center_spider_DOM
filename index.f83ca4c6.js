8325bd16f5373d47c200d6901766b3aa18d8cbf6
Rostyslav` <hrostyslav@ukr.net`>
2026-05-16 14:00:34 +0300

    make build

---
+++ index.f83ca4c6.js
@@ -0,0 +1,2 @@
+var t,e,o,f,c,d;t=document.querySelector(".wall"),o=(e=document.querySelector(".spider")).offsetWidth,f=e.offsetHeight,c=t.offsetWidth-20,d=t.offsetWidth-20,e.style.left="".concat(Math.round((c-o)/2),"px"),e.style.top="".concat(Math.round((d-f)/2),"px");
+//# sourceMappingURL=index.f83ca4c6.js.map
